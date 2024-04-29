ffd6abf3657ac4d25176932316649fe6cd19a1fc
makam <makam@MSI>
2024-04-29 01:03:02 -0400

    Feature 5 displays leaders

--- src/components/Feature5Component.jsx
+++ src/components/Feature5Component.jsx
@@ -10,23 +10,44 @@ import { Select,
     Td,
     Box,
 } from '@chakra-ui/react';
+import teamData from '../utils/leader';
      
 
 const Feature5Component = () => {
     const nbaApiKey = import.meta.env.VITE_REACT_APP_API_KEY;
     const [searchTerm, setSearchTerm] = useState('');
+
     const [stats1, setStats1] = useState([]);
     const [showStats1, setShowStats1] = useState(false);
+
+    const [team1Leader, setLeader1] = useState([]);
+
     const [stats2, setStats2] = useState([]);
     const [showStats2, setShowStats2] = useState(false);
+    
+    const [team2Leader, setLeader2] = useState([]);
+    const [firstTeamid, setFirst] = useState(-1);
 
     const handleSelectChange1 = async (event) => {
         const selectedValue = event.target.value;
         setSearchTerm(selectedValue);
+        setLeader1([]);
         if (selectedValue === "null") {
             setShowStats1(false);
             setStats1([]);
+            setLeader1([]);
             return;
+        }
+        const teamLeaderData = teamData.teams.find(team => team.id === parseInt(selectedValue));
+        if (teamLeaderData) {
+            setLeader1({
+                pointsLeader: teamLeaderData.pointsLeader,
+                ppg: teamLeaderData.ppg,
+                assistLeader: teamLeaderData.assistLeader,
+                apg: teamLeaderData.apg,
+                reboundLeader: teamLeaderData.reboundLeader,
+                rpg: teamLeaderData.rpg
+            });
         }
         const options = {
             method: 'GET',
@@ -97,15 +118,33 @@ const Feature5Component = () => {
             console.error(error);
             setShowStats1(false);
         }
+
+        // getTeam1Leaders(selectedValue);
     };
+
+    
+    
 
     const handleSelectChange2 = async (event) => {
         const selectedValue = event.target.value;
         setSearchTerm(selectedValue);
+        setLeader2([]);
         if (selectedValue === "null") {
             setShowStats2(false);
             setStats2([]);
+            setLeader2([]);
             return;
+        }
+        const teamLeaderData = teamData.teams.find(team => team.id === parseInt(selectedValue));
+        if (teamLeaderData) {
+            setLeader2({
+                pointsLeader: teamLeaderData.pointsLeader,
+                ppg: teamLeaderData.ppg,
+                assistLeader: teamLeaderData.assistLeader,
+                apg: teamLeaderData.apg,
+                reboundLeader: teamLeaderData.reboundLeader,
+                rpg: teamLeaderData.rpg
+            });
         }
         const options = {
             method: 'GET',
@@ -153,6 +192,7 @@ const Feature5Component = () => {
             assists /= games;
             blocks /= games;
             steals /= games;
+            plusMinus /= games;
             totalTurnovers /= games;
             const gameStats = {
                 Points: points.toFixed(1),
@@ -182,73 +222,75 @@ const Feature5Component = () => {
     return (
         <Box>
             <Flex gap="20px" mb="20px">
+                Team 1
                 <Select name="team1" id="team1" className="form-control" onChange={handleSelectChange1}>
                 <option value="null">Please Select A Team</option>
                 <option value="1">Atlanta Hawks</option>
                 <option value="2">Boston Celtics</option>
-                <option value="3">Brooklyn Nets</option>
-                <option value="4">Charlotte Hornets</option>
-                <option value="5">Chicago Bulls</option>
-                <option value="6">Cleveland Cavaliers</option>
-                <option value="7">Dallas Mavericks</option>
-                <option value="8">Denver Nuggets</option>
-                <option value="9">Detroit Pistons</option>
-                <option value="10">Golden State Warriors</option>
-                <option value="11">Houston Rockets</option>
-                <option value="12">Indiana Pacers</option>
-                <option value="13">Los Angeles Clippers</option>
-                <option value="14">Los Angeles Lakers</option>
-                <option value="15">Memphis Grizzlies</option>
-                <option value="16">Miami Heat</option>
-                <option value="17">Milwaukee Bucks</option>
-                <option value="18">Minnesota Timberwolves</option>
-                <option value="19">New Orleans Pelicans</option>
-                <option value="20">New York Knicks</option>
-                <option value="21">Oklahoma City Thunder</option>
-                <option value="22">Orlando Magic</option>
-                <option value="23">Philadelphia 76ers</option>
-                <option value="24">Phoenix Suns</option>
-                <option value="25">Portland Trail Blazers</option>
-                <option value="26">Sacramento Kings</option>
-                <option value="27">San Antonio Spurs</option>
-                <option value="28">Toronto Raptors</option>
-                <option value="29">Utah Jazz</option>
-                <option value="30">Washington Wizards</option>
+                <option value="4">Brooklyn Nets</option>
+                <option value="5">Charlotte Hornets</option>
+                <option value="6">Chicago Bulls</option>
+                <option value="7">Cleveland Cavaliers</option>
+                <option value="8">Dallas Mavericks</option>
+                <option value="9">Denver Nuggets</option>
+                <option value="10">Detroit Pistons</option>
+                <option value="11">Golden State Warriors</option>
+                <option value="14">Houston Rockets</option>
+                <option value="15">Indiana Pacers</option>
+                <option value="16">Los Angeles Clippers</option>
+                <option value="17">Los Angeles Lakers</option>
+                <option value="19">Memphis Grizzlies</option>
+                <option value="20">Miami Heat</option>
+                <option value="21">Milwaukee Bucks</option>
+                <option value="22">Minnesota Timberwolves</option>
+                <option value="23">New Orleans Pelicans</option>
+                <option value="24">New York Knicks</option>
+                <option value="25">Oklahoma City Thunder</option>
+                <option value="26">Orlando Magic</option>
+                <option value="27">Philadelphia 76ers</option>
+                <option value="28">Phoenix Suns</option>
+                <option value="29">Portland Trail Blazers</option>
+                <option value="30">Sacramento Kings</option>
+                <option value="31">San Antonio Spurs</option>
+                <option value="38">Toronto Raptors</option>
+                <option value="40">Utah Jazz</option>
+                <option value="41">Washington Wizards</option>
                 </Select>
             </Flex>
             <Flex direction={["column", "row"]} gap="20px">
+                Team 2
                 <Select name="team2" id="team2" className="form-control" onChange={handleSelectChange2}>
                 <option value="null">Please Select A Team</option>
                 <option value="1">Atlanta Hawks</option>
                 <option value="2">Boston Celtics</option>
-                <option value="3">Brooklyn Nets</option>
-                <option value="4">Charlotte Hornets</option>
-                <option value="5">Chicago Bulls</option>
-                <option value="6">Cleveland Cavaliers</option>
-                <option value="7">Dallas Mavericks</option>
-                <option value="8">Denver Nuggets</option>
-                <option value="9">Detroit Pistons</option>
-                <option value="10">Golden State Warriors</option>
-                <option value="11">Houston Rockets</option>
-                <option value="12">Indiana Pacers</option>
-                <option value="13">Los Angeles Clippers</option>
-                <option value="14">Los Angeles Lakers</option>
-                <option value="15">Memphis Grizzlies</option>
-                <option value="16">Miami Heat</option>
-                <option value="17">Milwaukee Bucks</option>
-                <option value="18">Minnesota Timberwolves</option>
-                <option value="19">New Orleans Pelicans</option>
-                <option value="20">New York Knicks</option>
-                <option value="21">Oklahoma City Thunder</option>
-                <option value="22">Orlando Magic</option>
-                <option value="23">Philadelphia 76ers</option>
-                <option value="24">Phoenix Suns</option>
-                <option value="25">Portland Trail Blazers</option>
-                <option value="26">Sacramento Kings</option>
-                <option value="27">San Antonio Spurs</option>
-                <option value="28">Toronto Raptors</option>
-                <option value="29">Utah Jazz</option>
-                <option value="30">Washington Wizards</option>
+                <option value="4">Brooklyn Nets</option>
+                <option value="5">Charlotte Hornets</option>
+                <option value="6">Chicago Bulls</option>
+                <option value="7">Cleveland Cavaliers</option>
+                <option value="8">Dallas Mavericks</option>
+                <option value="9">Denver Nuggets</option>
+                <option value="10">Detroit Pistons</option>
+                <option value="11">Golden State Warriors</option>
+                <option value="14">Houston Rockets</option>
+                <option value="15">Indiana Pacers</option>
+                <option value="16">Los Angeles Clippers</option>
+                <option value="17">Los Angeles Lakers</option>
+                <option value="19">Memphis Grizzlies</option>
+                <option value="20">Miami Heat</option>
+                <option value="21">Milwaukee Bucks</option>
+                <option value="22">Minnesota Timberwolves</option>
+                <option value="23">New Orleans Pelicans</option>
+                <option value="24">New York Knicks</option>
+                <option value="25">Oklahoma City Thunder</option>
+                <option value="26">Orlando Magic</option>
+                <option value="27">Philadelphia 76ers</option>
+                <option value="28">Phoenix Suns</option>
+                <option value="29">Portland Trail Blazers</option>
+                <option value="30">Sacramento Kings</option>
+                <option value="31">San Antonio Spurs</option>
+                <option value="38">Toronto Raptors</option>
+                <option value="40">Utah Jazz</option>
+                <option value="41">Washington Wizards</option>
                 </Select>
             </Flex>
 
@@ -256,6 +298,12 @@ const Feature5Component = () => {
             <Flex direction={["column", "row"]} gap="20px">
                 {showStats1 && (
                     <Box flex="1" boxShadow="md" p="6" rounded="md" bg="white">
+                        {/* <Box mb="4">
+                            <Text fontSize="lg" fontWeight="bold">Team 1 Leaders</Text>
+                            <Text>Points Leader: {team1Leader.pointsLeader} ({team1Leader.ppg} PPG)</Text>
+                            <Text>Assists Leader: {team1Leader.assistLeader} ({team1Leader.apg} APG)</Text>
+                            <Text>Rebounds Leader: {team1Leader.reboundLeader} ({team1Leader.rpg} RPG)</Text>
+                        </Box> */}
                         <Table size="sm">
                             <Thead>
                                 <Tr><Th>Stat</Th><Th>Value</Th></Tr>
@@ -266,6 +314,10 @@ const Feature5Component = () => {
                                         <Td>{key}</Td><Td>{value}</Td>
                                     </Tr>
                                 ))}
+                                <Tr fontSize="lg" fontWeight="bold">Team 1 Leaders</Tr>
+                                <Tr>Points Leader: {team1Leader.pointsLeader} ({team1Leader.ppg} PPG)</Tr>
+                                <Tr>Assists Leader: {team1Leader.assistLeader} ({team1Leader.apg} APG)</Tr>
+                                <Tr>Rebounds Leader: {team1Leader.reboundLeader} ({team1Leader.rpg} RPG)</Tr>
                             </Tbody>
                         </Table>
                     </Box>
@@ -282,6 +334,10 @@ const Feature5Component = () => {
                                         <Td>{key}</Td><Td>{value}</Td>
                                     </Tr>
                                 ))}
+                                <Tr fontSize="lg" fontWeight="bold">Team 2 Leaders</Tr>
+                                <Tr>Points Leader: {team2Leader.pointsLeader} ({team2Leader.ppg} PPG)</Tr>
+                                <Tr>Assists Leader: {team2Leader.assistLeader} ({team2Leader.apg} APG)</Tr>
+                                <Tr>Rebounds Leader: {team2Leader.reboundLeader} ({team2Leader.rpg} RPG)</Tr>
                             </Tbody>
                         </Table>
                     </Box>
---
+++ src/utils/leader.js
@@ -0,0 +1,275 @@
+const teamData ={
+    teams: [
+        {
+            id: 1,
+            pointsLeader: 'Trae Young',
+            ppg: 25.7,
+            assistLeader: 'Trae Young',
+            apg: 10.8,
+            reboundLeader: 'Clint Capela',
+            rpg: 10.6
+        },
+        {
+            id: 2,
+            pointsLeader: 'Jayson Tatum',
+            ppg: 26.9,
+            assistLeader: 'Derrick White',
+            apg: 5.2,
+            reboundLeader: 'Jayson Tatum',
+            rpg: 8.1
+        },
+        {
+            id: 4,
+            pointsLeader: 'Cam Thomas',
+            ppg: 22.5,
+            assistLeader: 'Dennis Schroder',
+            apg: 6.0,
+            reboundLeader: 'Nic Claxton',
+            rpg: 9.9
+        },
+        {
+            id: 5,
+            pointsLeader: 'LaMelo Ball',
+            ppg: 23.9,
+            assistLeader: 'LaMelo Ball',
+            apg: 8.0,
+            reboundLeader: 'Mark Williams',
+            rpg: 9.7
+        },
+        {
+            id: 6,
+            pointsLeader: 'DeMar DeRozan',
+            ppg: 24.0,
+            assistLeader: 'DeMar DeRozan',
+            apg: 5.3,
+            reboundLeader: 'Nikola Vucevic',
+            rpg: 10.5
+        },
+        {
+            id: 7,
+            pointsLeader: 'Donovan Mitchel',
+            ppg: 26.6,
+            assistLeader: 'Darius Garland',
+            apg: 6.5,
+            reboundLeader: 'Jarret Allen',
+            rpg: 10.5
+        },
+        {
+            id: 8,
+            pointsLeader: 'Luka Doncic',
+            ppg: 29.0,
+            assistLeader: 'Luka Doncic',
+            apg: 8.0,
+            reboundLeader: 'Luka Doncic',
+            rpg: 9.7
+        },
+        {
+            id: 9,
+            pointsLeader: 'Nikola Jokic',
+            ppg: 26.4,
+            assistLeader: 'Nikola Jokic',
+            apg: 9.0,
+            reboundLeader: 'Nikola Jokic',
+            rpg: 12.4
+        },
+        {
+            id: 10,
+            pointsLeader: 'Cade Cunningham',
+            ppg: 22.7,
+            assistLeader: 'Cade Cunningham',
+            apg: 7.5,
+            reboundLeader: 'Jalen Duren',
+            rpg: 11.6
+        },
+        {
+            id: 11,
+            pointsLeader: 'Stephen Curry',
+            ppg: 26.4,
+            assistLeader: 'Chris Paul',
+            apg: 6.8,
+            reboundLeader: 'Draymond Green',
+            rpg: 7.2
+        },
+        {
+            id: 14,
+            pointsLeader: 'Alperen Sengun',
+            ppg: 21.1,
+            assistLeader: 'Fred VanVleet',
+            apg: 8.1,
+            reboundLeader: 'Alperen Sengun',
+            rpg: 9.3
+        },
+        {
+            id: 15,
+            pointsLeader: 'Pascal Siakam',
+            ppg: 21.3,
+            assistLeader: 'Tyrese Haliburton',
+            apg: 10.9,
+            reboundLeader: 'Pascal Siakam',
+            rpg: 7.8
+        },
+        {
+            id: 16,
+            pointsLeader: 'Kawhi Leonard',
+            ppg: 23.7,
+            assistLeader: 'James Harden',
+            apg: 8.5,
+            reboundLeader: 'Ivic Zubac',
+            rpg: 9.2
+        },
+        {
+            id: 17,
+            pointsLeader: 'LeBron James',
+            ppg: 25.7,
+            assistLeader: 'LeBron James',
+            apg: 8.3,
+            reboundLeader: 'Anthony Davis',
+            rpg: 12.6
+        },
+        {
+            id: 19,
+            pointsLeader: 'Ja Morant',
+            ppg: 25.1,
+            assistLeader: 'Ja Morant',
+            apg: 8.1,
+            reboundLeader: 'Jordan Goodwin',
+            rpg: 8.0
+        },
+        {
+            id: 20,
+            pointsLeader: 'Jimmy Butler',
+            ppg: 20.8,
+            assistLeader: 'Jimmy Butler',
+            apg: 5.0,
+            reboundLeader: 'Bam Adebayo',
+            rpg: 10.4
+        },
+        {
+            id: 21,
+            pointsLeader: 'Giannis Antetokounmpo',
+            ppg: 30.4,
+            assistLeader: 'Damian Lillard',
+            apg: 7.0,
+            reboundLeader: 'Giannis Antetokounmpo',
+            rpg: 11.5
+        },
+        {
+            id: 22,
+            pointsLeader: 'Anthony Edwards',
+            ppg: 25.9,
+            assistLeader: 'Mike Conley',
+            apg: 5.9,
+            reboundLeader: 'Rudy Gobert',
+            rpg: 12.9
+        },
+        {
+            id: 23  ,
+            pointsLeader: 'Zion Williamson',
+            ppg: 22.9,
+            assistLeader: 'Brandon Ingram',
+            apg: 5.7,
+            reboundLeader: 'Jonas Valanciunas',
+            rpg: 8.8
+        },
+        {
+            id: 24  ,
+            pointsLeader: 'Jalen Brunson',
+            ppg: 28.7,
+            assistLeader: 'Jalen Brunson',
+            apg: 6.7,
+            reboundLeader: 'Julius Randle',
+            rpg: 9.2
+        },
+        {
+            id: 25  ,
+            pointsLeader: 'Shai Gilgeous-Alexander',
+            ppg: 30.1,
+            assistLeader: 'Shai Gilgeous-Alexander',
+            apg: 6.2,
+            reboundLeader: 'Chet Holmgren',
+            rpg: 7.9
+        },
+        {
+            id: 26  ,
+            pointsLeader: 'Paolo Banchero',
+            ppg: 22.6,
+            assistLeader: 'Paolo Banchero',
+            apg: 5.4,
+            reboundLeader: 'Wendell Carter Jr.',
+            rpg: 6.9
+        },
+        {
+            id: 27  ,
+            pointsLeader: 'Joel Embiid',
+            ppg: 34.7,
+            assistLeader: 'Tyrese Maxey',
+            apg: 6.2,
+            reboundLeader: 'Joel Embiid',
+            rpg: 11.0
+        },
+        {
+            id: 28  ,
+            pointsLeader: 'Kevin Durant',
+            ppg: 27.1,
+            assistLeader: 'Devin Booker',
+            apg: 6.9,
+            reboundLeader: 'Jusuf Nurkic',
+            rpg: 11.0
+        },
+        {
+            id: 29  ,
+            pointsLeader: 'Anferee Simons',
+            ppg: 22.6,
+            assistLeader: 'Anferee Simons',
+            apg: 5.5,
+            reboundLeader: 'Deandre Ayton',
+            rpg: 11.1
+        },
+        {
+            id: 30  ,
+            pointsLeader: "De'Aaron Fox",
+            ppg: 26.6,
+            assistLeader: 'Domantis Sabonis',
+            apg: 8.2,
+            reboundLeader: 'Domantis Sabonis',
+            rpg: 13.7
+        },
+        {
+            id: 31  ,
+            pointsLeader: "Victor Wembanyama",
+            ppg: 21.4,
+            assistLeader: 'Tre Jones',
+            apg: 6.2,
+            reboundLeader: 'Victor Wembanyama',
+            rpg: 10.6
+        },
+        {
+            id: 38  ,
+            pointsLeader: "RJ Barret",
+            ppg: 21.8,
+            assistLeader: 'Immanuel Quickley',
+            apg: 6.8,
+            reboundLeader: 'Jakob Poeltl',
+            rpg: 8.6
+        },
+        {
+            id: 40  ,
+            pointsLeader: "Lauri Markkanen",
+            ppg: 23.2,
+            assistLeader: 'Jordan Clarkson',
+            apg: 5.0,
+            reboundLeader: 'John Collins',
+            rpg: 8.5
+        },
+        {
+            id: 41  ,
+            pointsLeader: "Kyle Kuzma",
+            ppg: 22.2,
+            assistLeader: 'Tyus Jones',
+            apg: 7.3,
+            reboundLeader: 'Marvin Bagley III',
+            rpg: 8.1
+        }
+    ]
+}
+export default teamData;
