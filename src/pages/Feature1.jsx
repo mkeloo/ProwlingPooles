96824d46cdf908ee9d3726ade15f9f1f84373810
mkeloo <mkeloo@ufl.edu>
2024-04-28 20:22:46 -0400

    Feature 1 done.

--- src/pages/Feature1.jsx
+++ src/pages/Feature1.jsx
@@ -10,8 +10,17 @@ import {
   ModalCloseButton,
   Button,
   useDisclosure,
+  Image,
+  Flex,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
 } from '@chakra-ui/react';
-import playerData from '../utils/players.json';
+import playerData from '../utils/players';
 import allTeamsData from '../utils/Teams2023/allTeamsData';
 
 const Feature1 = () => {
@@ -31,125 +40,153 @@ const Feature1 = () => {
     return team ? team.id : null;
   };
 
-  const fetchPlayerId = async (lastName, teamId) => {
-    const options = {
-      method: 'GET',
-      url: 'https://api-nba-v1.p.rapidapi.com/players',
-      params: { name: lastName, team: teamId },
-      headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_API_KEY,
-        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
-      },
-    };
-
-    try {
-      const response = await axios.request(options);
-      if (
-        response.data &&
-        response.data.api &&
-        response.data.api.players.length > 0
-      ) {
-        const player = response.data.api.players.find(
-          (p) => p.lastName.toLowerCase() === lastName.toLowerCase()
-        );
-        if (player) {
-          return player.playerId;
-        } else {
-          console.error(
-            `No players found with the last name ${lastName} in team ${teamName}`
-          );
-          return null;
-        }
-      } else {
-        console.error(`No players found with the last name ${lastName}`);
-        return null;
-      }
-    } catch (error) {
-      console.error('Error fetching player ID:', error);
-      return null;
-    }
-  };
-
-  const fetchPlayerStats = async (playerId) => {
-    const options = {
-      method: 'GET',
-      url: 'https://api-nba-v1.p.rapidapi.com/players/statistics',
-      params: { id: playerId },
-      headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_API_KEY,
-        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
-      },
-    };
-
-    try {
-      const response = await axios.request(options);
-      setAdditionalStats(response.data.api.statistics);
-      onOpen();
-    } catch (error) {
-      console.error('Error fetching player statistics:', error);
-    }
-  };
-
   const handlePlayerSelect = async (player) => {
     setSelectedPlayer(player);
-    const lastName = player.name.split(' ').slice(-1).join(' ');
-    const teamId = getTeamIdByName(player.teamName);
+    const lastName = player.name.split(' ').pop();
+    const playerFromAllTeams = allTeamsData.find(
+      (p) => p.lastname.toLowerCase() === lastName.toLowerCase()
+    );
 
-    if (!teamId) {
-      console.error(`No team ID found for team name ${player.teamName}`);
+    if (!playerFromAllTeams) {
+      console.error(
+        `Player with last name ${lastName} not found in allTeamsData.`
+      );
       setAdditionalStats({
-        message: `No team ID found for team name ${player.teamName}`,
+        message: `Player with last name ${lastName} not found.`,
       });
       onOpen();
       return;
     }
 
-    const playerId = await fetchPlayerId(lastName, teamId);
-    if (playerId) {
-      await fetchPlayerStats(playerId);
-    } else {
-      setAdditionalStats(null);
-    }
+    const playerId = playerFromAllTeams.id;
+    await fetchPlayerStats(playerId);
     onOpen();
+  };
+
+  const convertToNumber = (str) => {
+    const number = parseFloat(str);
+    return isNaN(number) ? 0 : number;
+  };
+
+  const fetchPlayerStats = async (playerId) => {
+    const season = '2023';
+    const options = {
+      method: 'GET',
+      url: 'https://api-nba-v1.p.rapidapi.com/players/statistics',
+      params: { id: playerId, season: season },
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_API_KEY,
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      const stats = response.data.response;
+
+      if (stats && stats.length) {
+        let totals = {
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          blocks: 0,
+          steals: 0,
+          plusMinus: 0,
+          FG_percent: 0,
+          fieldGoalMakes: 0,
+          fieldGoalAttempts: 0,
+          FT_percent: 0,
+          freeThrowMakes: 0,
+          freeThrowAttempts: 0,
+          turnovers: 0,
+          games: stats.length,
+        };
+
+        stats.forEach((game) => {
+          totals.points += game.points;
+          totals.rebounds += game.totReb;
+          totals.assists += game.assists;
+          totals.blocks += game.blocks;
+          totals.steals += game.steals;
+          totals.plusMinus += convertToNumber(game.plusMinus);
+          totals.fieldGoalMakes += game.fgm;
+          totals.fieldGoalAttempts += game.fga;
+          totals.freeThrowMakes += game.ftm;
+          totals.freeThrowAttempts += game.fta;
+          totals.turnovers += game.turnovers;
+        });
+
+        let averages = {
+          Points: (totals.points / totals.games).toFixed(1),
+          Rebounds: (totals.rebounds / totals.games).toFixed(1),
+          Assists: (totals.assists / totals.games).toFixed(1),
+          Blocks: (totals.blocks / totals.games).toFixed(1),
+          Steals: (totals.steals / totals.games).toFixed(1),
+          FieldGoalPercentage: (
+            totals.fieldGoalMakes / totals.fieldGoalAttempts
+          ).toFixed(2),
+          FreeThrowPercentage: (
+            totals.freeThrowMakes / totals.freeThrowAttempts
+          ).toFixed(2),
+          Turnovers: (totals.turnovers / totals.games).toFixed(1),
+          PlusMinus: (totals.plusMinus / totals.games).toFixed(1),
+        };
+
+        setAdditionalStats(averages);
+        onOpen();
+      } else {
+        setAdditionalStats({
+          message:
+            'No statistics available for this player for season ' + season,
+        });
+        onOpen();
+      }
+    } catch (error) {
+      console.error('Error fetching player statistics:', error);
+      setAdditionalStats({
+        message: 'Failed to fetch statistics due to an error.',
+      });
+      onOpen();
+    }
   };
 
   const handleSearch = (e) => {
     e.preventDefault();
     const searchLower = searchTerm.trim().toLowerCase();
 
-    const filteredPlayers = allTeamsData
-      .filter((player) => {
-        const fullName = `${player.firstname} ${player.lastname}`.toLowerCase();
-        return fullName.includes(searchLower);
-      })
-      .map((player) => {
-        let teamInfo = { name: undefined, logo: undefined };
-
-        for (const divisionKey in playerData.NBA) {
-          if (playerData.NBA.hasOwnProperty(divisionKey)) {
-            const division = playerData.NBA[divisionKey];
-            const team = division.teams.find((t) => t.id === player.teamId);
-            if (team) {
-              teamInfo = { name: team.name, logo: team.image_url };
-              break;
+    let allPlayers = [];
+    if (playerData && playerData.NBA) {
+      Object.values(playerData.NBA).forEach((division) => {
+        if (division && division.teams) {
+          division.teams.forEach((team) => {
+            if (team && team.players) {
+              team.players.forEach((player) => {
+                if (player.name.toLowerCase().includes(searchLower)) {
+                  allPlayers.push({
+                    name: player.name,
+                    teamName: team.name,
+                    teamImage: team.image_url,
+                    imageUrl: player.image,
+                    position: player.position,
+                    age: player.age,
+                    height: player.height,
+                    weight: player.weight,
+                    college: player.college,
+                    salary: player.salary,
+                  });
+                }
+              });
             }
-          }
+          });
         }
-
-        return {
-          name: `${player.firstname} ${player.lastname}`,
-          teamName: teamInfo.name,
-          teamImage: teamInfo.logo,
-          playerId: player.id,
-          pos: player.leagues.standard.pos,
-        };
       });
+    }
 
-    setPlayers(filteredPlayers);
+    setPlayers(allPlayers);
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 ">
       <form onSubmit={handleSearch} className="flex mb-4">
         <input
           type="text"
@@ -165,25 +202,48 @@ const Feature1 = () => {
           Search
         </button>
       </form>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 m-4 ">
         {players.map((player, index) => (
           <div
             key={index}
-            className="p-4 shadow rounded hover:shadow-lg cursor-pointer transition-all duration-300"
+            className="p-4 m-8 shadow-lg rounded-xl hover:shadow-xl cursor-pointer transition duration-300 bg-slate-200"
             onClick={() => handlePlayerSelect(player)}
           >
-            <h3 className="font-bold text-xl mb-2">{player.name}</h3>
-            <img
-              src={player.teamImage}
-              alt={player.teamName}
-              className="w-32 h-32 object-cover mx-auto"
-            />
-            <p className="text-sm mb-2">
-              {player.pos} - {player.teamName}
-            </p>
+            <h3 className="font-bold text-xl mb-2 text-center">
+              {player.name}
+            </h3>
+            <div className="relative h-32 mb-2 ">
+              {player.imageUrl ? (
+                <img
+                  src={player.imageUrl}
+                  alt={player.name}
+                  className="w-full h-full object-contain mx-auto"
+                />
+              ) : (
+                // <div className="absolute inset-0 bg-slate-900 opacity-75 flex items-center justify-center rounded-lg">
+                //   <span className="text-white text-sm">
+                //     Image Not Available
+                //   </span>
+                // </div>
+                <img
+                  src="https://cdn.mos.cms.futurecdn.net/CPAhzgowLi2NtrP9HfVy9Y-1200-80.png"
+                  alt="Image Not Available"
+                  className="w-full h-full object-contain mx-auto rounded-lg"
+                />
+              )}
+            </div>
+            <div className="flex items-center justify-center space-x-2">
+              <p className="text-lg font-bold">{player.teamName}</p>
+              <img
+                src={player.teamImage}
+                alt={player.teamName + ' logo'}
+                className="w-12 h-12 object-cover"
+              />
+            </div>
           </div>
         ))}
       </div>
+
       {selectedPlayer && (
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
@@ -191,19 +251,69 @@ const Feature1 = () => {
             <ModalHeader>{selectedPlayer.name}</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <img
-                src={selectedPlayer.image_url || 'default-image.png'}
-                alt={selectedPlayer.name}
-                className="w-full h-auto"
-              />
+              <Flex direction="column" align="center" mb={4}>
+                <Image
+                  src={selectedPlayer.imageUrl || 'default-image.png'}
+                  alt={selectedPlayer.name}
+                  className="w-full h-auto mb-4 rounded-lg"
+                  fallbackSrc="https://www.logodesignlove.com/images/classic/nba-logo.jpg"
+                />
+                <Flex align="center" justify="center" mb={4}>
+                  <Text fontSize="xl" fontWeight="bold" mr={3}>
+                    {selectedPlayer.teamName}
+                  </Text>
+                  {selectedPlayer.teamImage ? (
+                    <Image
+                      src={selectedPlayer.teamImage}
+                      alt={`${selectedPlayer.teamName} logo`}
+                      boxSize="3rem"
+                    />
+                  ) : (
+                    <Flex
+                      align="center"
+                      justify="center"
+                      boxSize="3rem"
+                      borderWidth="1px"
+                      borderColor="gray.300"
+                      borderRadius="md"
+                      backgroundColor="gray.100"
+                    >
+                      <Text fontSize="sm" color="gray.500">
+                        Image Not Available
+                      </Text>
+                    </Flex>
+                  )}
+                </Flex>
+              </Flex>
               {additionalStats ? (
-                <div>
-                  <p>Points: {additionalStats.points}</p>
-                  <p>Rebounds: {additionalStats.rebounds}</p>
-                  <p>Assists: {additionalStats.assists}</p>
-                </div>
+                <Table variant="simple">
+                  <Thead>
+                    <Tr>
+                      <Th>Stat</Th>
+                      <Th isNumeric>Value</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {Object.entries(additionalStats).map(([stat, value]) => (
+                      <Tr key={stat}>
+                        <Td>{stat}</Td>
+                        <Td
+                          isNumeric
+                          fontWeight="extrabold"
+                          fontSize="lg"
+                          color="blue.600"
+                        >
+                          {value}
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
               ) : (
-                <p>No statistics available for this player.</p>
+                <Text>
+                  {additionalStats?.message ||
+                    'No additional statistics available.'}
+                </Text>
               )}
             </ModalBody>
             <ModalFooter>
